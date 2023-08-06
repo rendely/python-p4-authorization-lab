b085274d1132e514e75f90ac966bb55a1868d0de
matthew rendely <matthew.rendely@gmail.com>
2023-08-05 17:40:27 -0700

    done

--- server/app.py
+++ server/app.py
@@ -87,12 +87,24 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        if not session['user_id']:
+            return {}, 401
+        
+        articles = Article.query.filter_by(is_member_only=True).all()
+        return make_response([a.to_dict() for a in articles], 200)
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {}, 401
+
+        article = Article.query.filter_by(id = id).first()
+        return make_response(article.to_dict(), 200)
+
+
+        
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
